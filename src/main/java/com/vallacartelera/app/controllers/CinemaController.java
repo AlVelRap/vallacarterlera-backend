851292b29b42f3faf6564628026d6752891cc661
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2023-12-13 21:13:42 +0100

    docs: add swagger annotations to methods in CinemaController

--- src/main/java/com/vallacartelera/app/controllers/CinemaController.java
+++ src/main/java/com/vallacartelera/app/controllers/CinemaController.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,10 +24,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.vallacartelera.app.errors.models.ErrorMessage;
 import com.vallacartelera.app.models.Cinema;
 import com.vallacartelera.app.services.ICinemaService;
 import com.vallacartelera.app.views.Views;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -47,7 +54,14 @@ public class CinemaController {
 	/**
 	 * Return list of Cinemas, without Sessions
 	 */
-	@GetMapping("/cinemas")
+	@Operation(description = "Returns a List of all Cinemas in DB. Without List of Sessions.", summary = "List all Cinemas. Without Session's List", responses = {
+			@ApiResponse(responseCode = "200", description = "Success", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = Cinema.class))) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@GetMapping(path = "/cinemas", produces = MediaType.APPLICATION_JSON_VALUE)
 	@JsonView({ Views.GetAllCinemas.class })
 	public ResponseEntity<?> showAll() {
 		return new ResponseEntity<List<Cinema>>(cinemaService.findAll(), HttpStatus.OK);
@@ -56,7 +70,14 @@ public class CinemaController {
 	/**
 	 * Return data from one Cinema, with Sessions
 	 */
-	@GetMapping("/cinemas/{id}")
+	@Operation(description = "Returns a Cinema. With List of Sessions.", summary = "Show One Cinema. With Session's List.", responses = {
+			@ApiResponse(responseCode = "200", description = "Success", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@GetMapping(path = "/cinemas/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@JsonView({ Views.GetCinema.class })
 	public ResponseEntity<?> showCinema(@PathVariable Long id) {
 		return new ResponseEntity<Cinema>(cinemaService.findById(id), HttpStatus.OK);
@@ -65,7 +86,14 @@ public class CinemaController {
 	/**
 	 * Return a List of Cinemas that have One Movie. Without Sessions
 	 */
-	@GetMapping("/movies/{id}/cinemas")
+	@Operation(description = "Returns a list of all Cinemas for which a Movie has Sessions. Without List of Sessions.", summary = "List all Cinemas for Movie. Without Session's List", responses = {
+			@ApiResponse(responseCode = "200", description = "Success", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = Cinema.class))) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@GetMapping(path = "/movies/{id}/cinemas", produces = MediaType.APPLICATION_JSON_VALUE)
 	@JsonView({ Views.GetAllCinemas.class })
 	public ResponseEntity<?> showAllCinemasForMovie(@PathVariable Long id) {
 		return new ResponseEntity<List<Cinema>>(cinemaService.findAllByMovieId(id), HttpStatus.OK);
@@ -74,14 +102,28 @@ public class CinemaController {
 	/**
 	 * Return a Cinema that have a Movie. With Sessions
 	 */
-	@GetMapping("/movies/{movie_id}/cinemas/{cinema_id}")
+	@Operation(description = "Returns one Cinema for which a Movie has Sessions. With List of Sessions.", summary = "Show One Cinema for Movie. With Session's List.", responses = {
+			@ApiResponse(responseCode = "200", description = "Success", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@GetMapping(path = "/movies/{movie_id}/cinemas/{cinema_id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@JsonView({ Views.GetOneCinemasForMovie.class })
 	public ResponseEntity<?> showAllSessionsForCinemaForMovie(@PathVariable Long cinema_id,
 			@PathVariable Long movie_id) {
 		return new ResponseEntity<Cinema>(cinemaService.findByMovieIdAndCinemaId(cinema_id, movie_id), HttpStatus.OK);
 	}
 
-	@PostMapping("/cinemas")
+	@Operation(description = "Creates a Cinema.", summary = "Create Cinema.", responses = {
+			@ApiResponse(responseCode = "200", description = "Success", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@PostMapping(path = "/cinemas", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> create(@Valid @RequestBody Cinema cinema, BindingResult result) {
 		Map<String, Object> response = new HashMap<>();
@@ -100,7 +142,14 @@ public class CinemaController {
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 
-	@PutMapping("/cinemas/{id}")
+	@Operation(description = "Update a Cinema with its Id.", summary = "Update Cinema.", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@PutMapping(path = "/cinemas/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> update(@Valid @RequestBody Cinema cinema, BindingResult result) {
 		// TODO
 		return null;
@@ -109,7 +158,14 @@ public class CinemaController {
 	/**
 	 * Delete one Cinema with the id
 	 */
-	@DeleteMapping("/cinemas/{id}")
+	@Operation(description = "Delete a Cinema from DB.", summary = "Delete Cinema.", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@DeleteMapping(path = "/cinemas/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> deleteOne(@PathVariable Long id) {
 		Map<String, Object> response = new HashMap<>();
 
@@ -123,7 +179,14 @@ public class CinemaController {
 	/**
 	 * Delete All Cinemas from DB
 	 */
-	@DeleteMapping("/cinemas")
+	@Operation(description = "Delete all Cinemas from DB.", summary = "Delete All Cinemas.", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Cinema.class)) }),
+			@ApiResponse(responseCode = "500", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }),
+			@ApiResponse(responseCode = "404", content = {
+					@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessage.class)) }) })
+	@DeleteMapping(path = "/cinemas", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> deleteAll() {
 		Map<String, Object> response = new HashMap<>();
 
