05eccdf02dbb7dc402ca2234ac0c3f957fcee7b3
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2023-12-26 19:21:35 +0100

    fix: add query to avoid show all sessions from cinema instead only sessions for movie

--- src/main/java/com/vallacartelera/app/dao/ICinemaDao.java
+++ src/main/java/com/vallacartelera/app/dao/ICinemaDao.java
@@ -3,6 +3,7 @@ package com.vallacartelera.app.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vallacartelera.app.models.Cinema;
@@ -11,5 +12,6 @@ import com.vallacartelera.app.models.Cinema;
 public interface ICinemaDao extends JpaRepository<Cinema, Long> {
 	public List<Cinema> findBySessions_Movie_Id(Long id);
 
+	@Query(value = "select distinct c from Cinema c join fetch c.sessions s where c.id = ?1 and s.movie.id = ?2")
 	public Cinema findByIdAndSessions_Movie_Id(Long cinema_id, Long movie_id);
 }
