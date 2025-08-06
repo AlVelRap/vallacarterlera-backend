64295351b2cadab8c1c2af7527bf8d4aafcbf774
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2025-08-06 13:58:02 +0200

    feat: create basic user class Usuario for auth

---
+++ src/main/java/com/vallacartelera/app/auth/models/Usuario.java
@@ -0,0 +1,72 @@
+package com.vallacartelera.app.auth.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 30, unique = true)
+	@NotNull
+	private String username;
+
+	@NotNull
+	@Column(length = 60)
+	private String password;
+
+	@NotNull
+	@Column(length = 60)
+	private String salt;
+
+	private boolean enabled;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
